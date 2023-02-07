a6e79f69dabb0ee0472cc304920ea552e6c510bb
Eber <52986565+doneber@users.noreply.github.com>
2023-02-06 20:51:54 -0400

    Add responsive mobile

--- frontend/src/components/BannerComponent.vue
+++ frontend/src/components/BannerComponent.vue
@@ -11,12 +11,23 @@ header {
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  gap: .3rem;
+  gap: .8rem;
 }
 
 header h1 {
   font-size: 3rem;
   margin: 0;
+  text-align: center;
+  line-height: 1;
 }
 
+header p {
+  text-align: center;
+}
+
+@media (max-width: 400px) {
+  header h1 {
+    font-size: 2.5rem;
+  }
+}
 </style>
