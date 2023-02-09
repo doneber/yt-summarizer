c35d0969d8d2a6c946ddc010ecf1dc425fd4bd5f
Eber <52986565+doneber@users.noreply.github.com>
2023-02-09 09:46:07 -0400

    Fix possible conflicts to deploy with vercel

--- api/app2.js
+++ api/app2.js
@@ -11,11 +11,11 @@ app.use(express.static(path.join(__dirname, '..', 'frontend', 'dist')))
 const getSubtitles = require('youtube-captions-scraper').getSubtitles
 
 // endpoints
-app.get('/api', (req, res) => {
-  res.status(200).json({ message: 'Welcome to the API' })
+app.get('/api2', (req, res) => {
+  res.status(200).json({ message: 'Welcome to the API2' })
 })
 
-app.get('/api/:videoId', async (req, res) => {
+app.get('/api2/:videoId', async (req, res) => {
   await getSubtitles({
     videoID: req.params.videoId,
     lang: 'es'
